75ec8709be4547ccdd0e6bca5b53f70376282b87
franzap <_@franzap.com>
2026-09-19 23:50:09 -0300

    Run in port 3338

--- cmd/dummy/main.go
+++ cmd/dummy/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("addr", "localhost:8080", "listen address")
+	addr := flag.String("addr", "localhost:3338", "listen address")
 	flag.Parse()
 
 	if err := run(*addr); err != nil {
